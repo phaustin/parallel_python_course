3f65e65639feaa2a3b4ef57e1dee18de7b003478
Phil Austin <paustin@eos.ubc.ca>
2017-06-22 00:21:46 -0500

    backout

--- code_examples/cffi_funs/recipe/build.sh
+++ code_examples/cffi_funs/recipe/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+module load gcc/5.2.0-experimental
+cmake -DCMAKE_CXX_COMPILER=/global/software/gcc-5.2.0-rpath/bin/g++ -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR
 make install
