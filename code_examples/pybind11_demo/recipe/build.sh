1c9e7908108f7017c3249d0b613e35a47295694e
Phil Austin <paustin@eos.ubc.ca>
2017-06-22 16:15:26 -0700

    grex-specific build.sh

--- code_examples/pybind11_demo/recipe/build.sh
+++ code_examples/pybind11_demo/recipe/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-unset MACOSX_DEPLOYMENT_TARGET
-${PYTHON} setup.py install;
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+module load gcc/5.2.0-experimental
+cmake -DCMAKE_CXX_COMPILER=/global/software/gcc-5.2.0-rpath/bin/g++ -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR
+make install
