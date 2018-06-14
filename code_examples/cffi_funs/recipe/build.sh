9b06d3421c412966537b91e15d801fd722570e07
Phil Austin <paustin@eos.ubc.ca>
2018-06-13 20:57:43 -0700

    test

--- code_examples/cffi_funs/recipe/build.sh
+++ code_examples/cffi_funs/recipe/build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-if [ -f /etc/bashrc ]; then
-	. /etc/bashrc
-fi
-module load gcc/5.2.0-experimental
-cmake -DCMAKE_CXX_COMPILER=/global/software/gcc-5.2.0-rpath/bin/g++ -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR
+cmake -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR
 make install
