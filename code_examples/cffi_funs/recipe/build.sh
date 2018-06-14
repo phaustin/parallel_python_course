cb2ef8ca9f97fd8b3db1d4b3315444d2faba77ba
Phil Austin <paustin@eos.ubc.ca>
2018-06-13 21:18:51 -0700

    working on linux

--- code_examples/cffi_funs/recipe/build.sh
+++ code_examples/cffi_funs/recipe/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cmake -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR
+export GXX=/home/phil/ana36/bin/x86_64-conda_cos6-linux-gnu-g++
+export CXX=$GXX
+cmake -DCMAKE_CXX_COMPILER=$GXX -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR
 make install
